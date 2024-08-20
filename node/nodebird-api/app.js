ef7e89f2392fefc329601b284612a408fd1d662c
back <bjh59460@gmail.com>
2024-08-20 09:15:30 +0900

    ...

--- node/nodebird-api/app.js
+++ node/nodebird-api/app.js
@@ -24,22 +24,22 @@ app.set('port', process.env.PORT || 8002);
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie:{
-        httpOnly: true,
-        secure: false,
-    },
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
-
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/v1',v1);
+app.use('/v1', v1);
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
 
